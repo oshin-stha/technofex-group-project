d043dc25c58b2452369f3c4faa25ba825931ffe7
oshin_stha <oshinshrestha2055@gmail.com>
2024-02-18 12:02:42 +0545

    #ticket1234 : husky check 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'group-project';
-  ngOnInit(){
+  ngOnInit() {
     console.log('this must throw error');
   }
 }
