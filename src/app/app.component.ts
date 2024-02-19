5139f7412a5ffe79837a9a27a4cc18835f618acc
oshin_stha <oshinshrestha2055@gmail.com>
2024-02-19 15:55:07 +0545

    #ticket432 ts file checking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'group-project';
-  ngOnInit() {
-    this.getData();
-  }
+  ngOnInit() {}
   getData() {
     console.log('djfghohdnxv');
   }
